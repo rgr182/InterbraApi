b4b48af06fdb7332aa2d105ea7d478df0ce18970
LIBERO\AZUL <lic.cesarortiz@hotmail.com>
2024-11-11 15:31:04 -0600

    *Product controller added *Product Entity added *Product Repository added

---
+++ InterbraApi/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using InterbraApi.Domain.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterbraApi.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository productRrepository)
+        {
+            _productRepository = productRrepository;
+        }
+
+        [HttpPost("SaveProduct")]
+        public IActionResult SaveProduct()
+        {
+            return Ok();
+        }
+    }
+}
+
+
+
+
---
+++ InterbraApi/Domain/Repository/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using InterbraApi.Domain.Entities;
+using InterbraApi.Domain.Model;
+using System.Data;
+using System.Data.Common;
+
+namespace InterbraApi.Domain.Repository
+{
+
+    public interface IProductRepository
+    {
+        public Product SaveProduct(Product Product);
+    }
+
+    public class ProductRepository : IProductRepository
+    {
+         
+        private readonly IServiceProvider _serviceProvider;
+
+        public ProductRepository(IServiceProvider serviceProvider) 
+        {
+         _serviceProvider = serviceProvider;
+        }
+        private IDbConnection CreateConnection()
+        {
+            return _serviceProvider.GetRequiredService<IDbConnection>();
+        }
+        public Product SaveProduct(Product Product)
+        {
+
+
+            using (var dbConnection = CreateConnection())
+            {
+                string sql = "INSERT INTO [Product] (Name, Description, Size, Quantity]) " +
+                                 "VALUES (@Name, @Description, @Size, @Quantity);";
+
+                dbConnection.Execute(sql, new
+                {
+                    Name = Product.Name,
+                    Description = Product.Description,
+                    Size = Product.Size,
+                    Quantity = Product.Quantity,
+                });
+                return Product;
+
+
+            }
+        }
+    }
+}
