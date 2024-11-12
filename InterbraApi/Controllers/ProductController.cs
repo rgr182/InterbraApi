9f9f7915c85ee9c9ab75808d915663b71c6268cc
LIBERO\AZUL <lic.cesarortiz@hotmail.com>
2024-11-12 15:53:11 -0600

    *ProductId change to private *Schemas added for post

--- InterbraApi/Controllers/ProductController.cs
+++ InterbraApi/Controllers/ProductController.cs
@@ -7,25 +7,33 @@ namespace InterbraApi.Controllers
     public class ProductController : Controller
     {
         private readonly IProductRepository _productRepository;
-        public ProductController(IProductRepository productRrepository)
+
+        // Constructor initializes the ProductController with an instance of IProductRepository
+        public ProductController(IProductRepository productRepository)
         {
-            _productRepository = productRrepository;
+            _productRepository = productRepository;
         }
 
+        /// <summary>
+        /// Method created for saving products. This may include bras and panties from Interbra.
+        /// </summary>
+        /// <param name="newproduct">Represents the model created for Interbra underwear products</param>
+        /// <returns>Returns an accurate status code based on the success or failure of the request</returns>
         [HttpPost("SaveProduct")]
-        public IActionResult SaveProduct()
+        public IActionResult SaveProduct([FromBody] Product newproduct)
         {
-            return Ok();
+            return Ok(_productRepository.SaveProduct(newproduct));
         }
 
+        /// <summary>
+        /// Retrieves a specific number of products.
+        /// </summary>
+        /// <param name="amount">The number of products to retrieve</param>
+        /// <returns>Returns the top products based on the specified amount</returns>
         [HttpGet("GetProducts")]
-        public IActionResult SaveProduct(int amount)
+        public IActionResult GetProducts(int amount)
         {
             return Ok(_productRepository.GetTopProducts(amount));
         }
     }
 }
-
-
-
-
--- InterbraApi/Domain/Entities/Product.cs
+++ InterbraApi/Domain/Entities/Product.cs
@@ -2,7 +2,7 @@
 {
     public class Product
     {
-        public int ProductId { get; set; }
+        private int ProductId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Size { get; set; }
